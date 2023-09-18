86126e11ad2e94561353d4b6bc40bd2bed8ad633
luciel53 <l.leroty@gmail.com>
2023-09-18 20:04:31 +0200

    fixed shebang task 0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+ #!/usr/bin/env python3
 """
 Write an asynchronous coroutine that takes in an integer argument
 (max_delay, with a default value of 10) named wait_random that waits for a
@@ -7,7 +7,6 @@ eventually returns it.
 
 Use the random module.
 """
-
 
 import random
 import asyncio
