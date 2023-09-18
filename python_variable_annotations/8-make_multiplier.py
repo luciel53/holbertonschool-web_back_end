631199b4d2fb56733547726ee97c22adb13f0c17
luciel53 <l.leroty@gmail.com>
2023-09-18 13:20:34 +0200

    task 8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function make_multiplier that takes a float multiplier
+as argument and returns a function that multiplies a float by multiplier.
+"""
+
+from typing import Union, List, Tuple, Callable
+import math
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ to_kv function """
+    def mult_float(n: float) -> float:
+        return n * multiplier
+    return mult_float
