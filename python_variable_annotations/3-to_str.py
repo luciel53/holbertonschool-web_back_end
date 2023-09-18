cacdea72d41ddc6ccc36244292e3ee345ee6698b
luciel53 <l.leroty@gmail.com>
2023-09-18 11:22:51 +0200

    task 4

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -4,9 +4,7 @@ Write a type-annotated function to_str that takes a float n as argument and
 returns the string representation of the float.
 """
 
-import math
-
 
 def to_str(n: float) -> str:
-    """ Floor function """
+    """ to_str function """
     return str(n)
