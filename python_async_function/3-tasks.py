84ec48d1171e6cc16244b39457d4bba17a239953
luciel53 <l.leroty@gmail.com>
2023-09-19 10:21:52 +0200

    task 3 fix return

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -8,13 +8,12 @@ returns a asyncio.Task.
 """
 
 import asyncio
-import random
 from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay):
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """ task_wait_random function """
     task = asyncio.create_task(wait_random(max_delay))
     return task
