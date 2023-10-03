19214937a452c3ab48caa714ae4638593d3c826e
luciel53 <l.leroty@gmail.com>
2023-10-03 14:37:45 +0200

    task 0

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Write a function called filter_datum that returns the log message obfuscated:
+
+    Arguments:
+        fields: a list of strings representing all fields to obfuscate
+        redaction: a string representing by what the field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character is separating all
+        fields in the log line (message)
+    The function should use a regex to replace occurrences of certain field
+    values.
+    filter_datum should be less than 5 lines long and use re.sub to perform
+    the substitution with a single regex.
+
+"""
+
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator:
+                 str) -> str:
+    """ filter fatum function """
+    msg_cp = message
+    for field in fields:
+        pattern = field + '=.*?' + separator
+        msg_cp = re.sub(pattern, field + '=' + redaction + separator, msg_cp)
+
+    return (msg_cp)
