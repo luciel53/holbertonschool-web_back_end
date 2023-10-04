c873d7564b42e8566f0da694d64b091f1eb0862e
luciel53 <l.leroty@gmail.com>
2023-10-04 20:16:52 +0200

    task 3

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """ Class Auth """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Require function """
+        return (False)
+
+
+    def authorization_header(self, request=None) -> str:
+        """ authorization header function """
+        if request is None:
+            return (None)
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user function """
+        if request is None:
+            return (None)
