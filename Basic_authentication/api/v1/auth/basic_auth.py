4412dbf98a5fe68c25ba21256a399370089b0430
luciel53 <l.leroty@gmail.com>
2023-10-05 18:51:56 +0200

    task 10

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,6 +8,7 @@ from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -57,3 +58,24 @@ class BasicAuth(Auth):
 
         split_code = decoded_base64_authorization_header.split(':')
         return (split_code[0], split_code[1])
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ user_object_from_credentials method """
+        if not isinstance(user_email, str) or user_email is None:
+            return None
+
+        if not isinstance(user_pwd, str) or user_pwd is None:
+            return None
+
+        # Search for User instance on email
+        user = User.search({"email": user_email})
+
+        if not user:
+            return None
+
+        # function is_valid_password in user.py
+        if not user[0].is_valid_password(user_pwd):
+            return None
+
+        return user[0]
