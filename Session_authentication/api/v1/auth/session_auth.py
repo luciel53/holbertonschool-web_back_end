7eafe27bccf5d19fa3d985a099ed93772bcac53a
luciel53 <l.leroty@gmail.com>
2023-10-08 21:04:40 +0200

    task 2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -9,8 +9,23 @@ from typing import List, TypeVar
 from api.v1.auth.auth import Auth
 from base64 import b64decode
 from models.user import User
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ Basic Authentication class """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ create a session method """
+        if user_id is None:
+            return None
+
+        if type(user_id) is not str:
+            return None
+
+        session_id = uuid4()
+
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+
+        return session_id
