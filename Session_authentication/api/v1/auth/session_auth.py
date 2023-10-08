3ae3461ba61db5583392915528742e19f7884e72
luciel53 <l.leroty@gmail.com>
2023-10-08 21:25:24 +0200

    task 3

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -4,10 +4,7 @@ class BasicAuth that inherits from Auth
 """
 
 
-from flask import request
-from typing import List, TypeVar
 from api.v1.auth.auth import Auth
-from base64 import b64decode
 from models.user import User
 from uuid import uuid4
 
@@ -24,8 +21,19 @@ class SessionAuth(Auth):
         if type(user_id) is not str:
             return None
 
-        session_id = uuid4()
+        session_id = str(uuid4())
 
         SessionAuth.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ user_id for session id method """
+        if session_id is None:
+            return None
+
+        if type(session_id) is not str:
+            return None
+
+        return SessionAuth.user_id_by_session_id.get(session_id)
