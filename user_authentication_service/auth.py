1939e0214518706a734db3b6d05999a2a272588c
luciel53 <l.leroty@gmail.com>
2023-10-10 16:42:47 +0200

    task 9

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -30,6 +30,12 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
+def _generate_uuid() -> str:
+    """ function that returns a string representation of a new uuid4 """
+    new_uuid = str(uuid4())
+    return new_uuid
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -69,9 +75,3 @@ class Auth:
 
         except Exception:
             return False
-
-    def _generate_uuid() -> str:
-        """ function that returns a string representation of a new uuid4 """
-        uuid = str(uuid4())
-
-        return uuid
