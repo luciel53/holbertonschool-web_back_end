71513ee7698e5706b592d6d19b098b2097b6597b
luciel53 <l.leroty@gmail.com>
2023-10-11 11:24:24 +0200

    task 11

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -38,5 +38,36 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'])
+def login():
+    """
+    login function to respond to the POST /sessions route.
+
+The request is expected to contain form data with "email" and a "password"
+fields.
+
+If the login information is incorrect, use flask.abort to respond with a
+401 HTTP status.
+
+Otherwise, create a new session for the user, store it the session ID as a
+cookie with key "session_id" on the response and return a JSON payload of
+the form
+    """
+    # get email and password
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # check login informations
+    if AUTH.valid_login(email, password):
+        # create session for user and generate a new id
+        session_id = AUTH.create_session(email=email)
+        response = jsonify({"email": email, "message": "logged in"})
+        # store session id inasmuch as cookie
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
