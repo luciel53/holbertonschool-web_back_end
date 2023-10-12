6e822eef701b61a91c770845516d2f4066c56f3e
luciel53 <l.leroty@gmail.com>
2023-10-12 20:29:24 +0200

    task 19

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -126,18 +126,36 @@ def get_reset_password_token():
         abort(403)
 
 
-@app.route('/reset_password', methods=['PUT'])
-def update_password():
-    """ def update password """
-    email = request.form.get('email')
-    reset_token = request.form.get('reset_token')
-    new_password = request.form.get('new_password')
-    try:
-        AUTH.update_password(reset_token, new_password)
-        return jsonify({"email": email, "message": "Password updated"})
-    except Exception:
-        abort(403)
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """ PUT /reset_password
+    Updates user password
+    Email, reset_token and new_password fields in x-www-form-urlencoded request
+    Return:
+      - JSON payload
+    """
+    form_data = request.form
 
+    if "email" not in form_data:
+        return jsonify({"message": "email required"}), 400
+    if "reset_token" not in form_data:
+        return jsonify({"message": "reset_token required"}), 400
+    if "new_password" not in form_data:
+        return jsonify({"message": "new_password required"}), 400
+    else:
+
+        email = request.form.get("email")
+        reset_token = request.form.get("reset_token")
+        new_pswd = request.form.get("new_password")
+
+        try:
+            AUTH.update_password(reset_token, new_pswd)
+            return jsonify({
+                "email": email,
+                "message": "Password updated"
+            }), 200
+        except ValueError:
+            abort(403)
 
 
 if __name__ == "__main__":
