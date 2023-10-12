95eac3ae12844731ea0dd3d8fa2be8fba56a2394
luciel53 <l.leroty@gmail.com>
2023-10-12 11:42:01 +0200

    task 15 add msg raise

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -133,13 +133,12 @@ class Auth:
         # Find the user corresponding to the email
         user = self._db.find_user_by(email=email)
 
-        if user is None:
-            raise ValueError
-
-        else:
+        if user:
             # generate a new uuid
             new_uuid = _generate_uuid()
 
             # update user's reset_token database field
             self._db.update_user(user.id, reset_token=new_uuid)
             return new_uuid
+        else:
+            raise ValueError("User DNE")
