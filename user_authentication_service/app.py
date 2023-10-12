c851e4006222b9ac600b7339ec08aa953d7cdbed
luciel53 <l.leroty@gmail.com>
2023-10-12 19:50:19 +0200

    task 19 add value error

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -155,7 +155,7 @@ def update_password():
         AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
 
-    except Exception:
+    except ValueError:
         abort(403)
 
 
