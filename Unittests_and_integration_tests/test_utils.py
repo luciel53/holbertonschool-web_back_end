c5bb42dfefbf3d2a556f77f9d530989405d96e39
luciel53 <l.leroty@gmail.com>
2023-10-16 17:15:49 +0200

    task 1

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -19,15 +19,29 @@ access_nested_map = __import__('utils').access_nested_map
 class TestAccessNestedMap(unittest.TestCase):
     """ TestAccessNestedMap class that inherits from unittest.TestCase """
     @parameterized.expand([
+        # contains dict, path and expected value
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
 
     def test_access_nested_map(self, nested_map, path, expected_result):
-        """ test access_nested_map """
+        """ checks if the result is equal to the expected result"""
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_result)
+
+    @parameterized.expand([
+        # contains dict, path and expected value
+        ({}, ("a",), ""),
+        ({"a": 1}, ("a", "b",), ""),
+    ])
+
+    def test_access_nested_map_exception(self, nested_map, path, expected_msg):
+        """ check assertRaises exception """
+        with self.assertRaises(Exception):
+            access_nested_map(nested_map, path)
+        exception_msg = "Error msg"
+        self.assertEqual(exception_msg, expected_msg)
 
 
 if __name__ == '__main__':
