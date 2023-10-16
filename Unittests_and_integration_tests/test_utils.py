43ef4071fdd3572bb60ed0dd0282355f556f591c
luciel53 <l.leroty@gmail.com>
2023-10-16 16:26:28 +0200

    task 0

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -19,11 +19,11 @@ access_nested_map = __import__('utils').access_nested_map
 class TestAccessNestedMap(unittest.TestCase):
     """ TestAccessNestedMap class that inherits from unittest.TestCase """
     @parameterized.expand([
-        ({"a": 1}, ("a",)),
-        ({"a": {"b": 2}}, ("a",)),
-        ({"a": {"b": 2}}, ("a", "b")),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
 
     def test_access_nested_map(self, nested_map, path):
-        """ test if nested map is empty """
+        """ test access_nested_map """
         self.assertEqual(access_nested_map(nested_map, path))
