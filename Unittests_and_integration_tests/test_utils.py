5dc7a713b4cc3a1d9497870f04934d0c46f898df
luciel53 <l.leroty@gmail.com>
2023-10-16 16:48:52 +0200

    task 0

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -20,10 +20,15 @@ class TestAccessNestedMap(unittest.TestCase):
     """ TestAccessNestedMap class that inherits from unittest.TestCase """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), 2),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
 
-    def test_access_nested_map(self, nested_map, path):
+    def test_access_nested_map(self, nested_map, path, expected_result):
         """ test access_nested_map """
-        self.assertEqual(access_nested_map(nested_map, path))
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
