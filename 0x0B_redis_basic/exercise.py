e29728891a153aeadbcfb18d718121feff6da261
luciel53 <l.leroty@gmail.com>
2023-10-30 19:23:49 +0100

    task 2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,6 +4,20 @@
 import redis
 import uuid
 from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """ count calls method decorator """
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ count how many times methods of the Cache class are called """
+        method_name = method.__qualname__  # store the name of the method
+        self._redis.incr(method_name)  # increment counter associated to name
+        value = method(self, *args, **kwargs)  # call original meth & store it
+        return value
+    return wrapper
 
 
 class Cache:
@@ -13,14 +27,15 @@ class Cache:
         self._redis = redis.Redis()  # create an instance of Redis class
         self._redis.flushdb()  # clean completely the db
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ method that generates a random key and store data in database """
         random_key = str(uuid.uuid4())  # generate random key
         self._redis.set(random_key, data)  # associate the value to the key
         return random_key
 
-    def get(self, key: str, fn: Callable=None) -> Union[str, Callable, bytes, int,
-                                                   float]:
+    def get(self, key: str, fn: Callable = None) -> Union[str, Callable, bytes,
+                                                          int, float]:
         """ method that returns the value of the given key from the db """
         value = self._redis.get(key)  # get the value with the key
         if fn:
@@ -32,7 +47,7 @@ class Cache:
         return self._redis.get(key)
 
     def get_int(self, key: str) -> int:
-        """ get int """
+        """ parametrize value from redis to int """
         numb = int(self._redis.get(key))
         return numb
 
