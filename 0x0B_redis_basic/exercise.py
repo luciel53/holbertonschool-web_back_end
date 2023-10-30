2541d17f55a38510260d4868401061615f39ff71
luciel53 <l.leroty@gmail.com>
2023-10-30 12:32:31 +0100

    task 0

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Exercise file """
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """ Cache class """
+    def __init__(self):
+        """ init method """
+        self._redis = redis.Redis()  # create an instance of Redis class
+        self._redis.flushdb()  # clean completely the db
+
+    def store(self, data) -> Union(str, bytes, int, float):
+        """ method that generates a random key"""
+        random_key = str(uuid.uuid4())  # generate random key
+        self._redis.set(random_key, data)  # associate the value to the key
+        return random_key
