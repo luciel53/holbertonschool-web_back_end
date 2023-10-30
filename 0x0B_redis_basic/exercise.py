5496248b7ebb946b1a88a9ed753fd5ed5dfbefa7
luciel53 <l.leroty@gmail.com>
2023-10-30 19:54:53 +0100

    task 2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -50,16 +50,3 @@ class Cache:
         """ parametrize value from redis to int """
         numb = int(self._redis.get(key))
         return numb
-
-
-cache = Cache()
-
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
