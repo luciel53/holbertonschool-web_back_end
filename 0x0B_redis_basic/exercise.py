dbb1eff3fa5ed8652d590b0eff9e9b18cd7eb22b
luciel53 <l.leroty@gmail.com>
2023-10-30 13:07:08 +0100

    task 0 replace []

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -13,7 +13,7 @@ class Cache:
         self._redis = redis.Redis()  # create an instance of Redis class
         self._redis.flushdb()  # clean completely the db
 
-    def store(self, data: Union(str, bytes, int, float)) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ method that generates a random key"""
         random_key = str(uuid.uuid4())  # generate random key
         self._redis.set(random_key, data)  # associate the value to the key
