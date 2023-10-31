29904609080d8dfe2e9f2302e92db6603ca9a953
luciel53 <l.leroty@gmail.com>
2023-10-31 16:27:43 +0100

    task 3

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -20,6 +20,27 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """ Store the historyof in inputs and outputs for a function """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        #  Create keys for input and output lists
+        input = "{}:inputs".format(method.__qualname__)
+        output = "{}:outputs".format(method.__qualname__)
+
+        data = str(args)  # convert args in str
+
+        self._redis.rpush(input, data)  # add data to input list
+
+        value = method(self, *args, **kwargs)  # exec originale function
+
+        self._redis.rpush(output, value)  # add value to output list
+
+        return value
+
+    return wrapper
+
+
 class Cache:
     """ Cache class """
     def __init__(self):
@@ -27,6 +48,7 @@ class Cache:
         self._redis = redis.Redis()  # create an instance of Redis class
         self._redis.flushdb()  # clean completely the db
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ method that generates a random key and store data in database """
