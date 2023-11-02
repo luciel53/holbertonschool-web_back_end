c0a1f7f7ebe6994ac4abbaf71b06a68507d7f39f
luciel53 <l.leroty@gmail.com>
2023-11-02 17:15:20 +0100

    task 1

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,7 @@
 -- add country, enumeration of countries: US, CO and TN, never null
-
 CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255)
-	country ENUM('US', 'CO', 'TN') NOT NULL,
+	country ENUM('US', 'CO', 'TN') NOT NULL
 	);
