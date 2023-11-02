8cc6d2facb1d2d4f00f80908f079b52993e1762a
luciel53 <l.leroty@gmail.com>
2023-11-02 19:54:40 +0100

    task 4

--- MySQL_Advanced/4-store.sql
+++ MySQL_Advanced/4-store.sql
@@ -1,6 +1,6 @@
 -- script that creates a trigger that decreases the quantity of an item after adding a new order.
 CREATE TRIGGER decrease_quantity
 AFTER INSERT ON orders
+FOR EACH ROW
 UPDATE items
-FOR EACH ROW SET @quantity = @quantity - @NEW.NUMBER
-WHERE name = NEW.item_name;
+SET @quantity = @quantity - @NEW.NUMBER;
