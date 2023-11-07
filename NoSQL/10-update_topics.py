a2b9730a301119035231cce57f1572eb7f047365
luciel53 <l.leroty@gmail.com>
2023-11-07 19:45:23 +0100

    task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Python function that changes all topics of a school document based on
+    the name:
+
+    Prototype: def update_topics(mongo_collection, name, topics):
+    mongo_collection will be the pymongo collection object
+    name (string) will be the school name to update
+    topics (list of strings) will be the list of topics approached in
+    the school
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """ function taht changes all topics of a document based on name """
+    found_by_name = mongo_collection.find(name)
+    updated = found_by_name.update(topics)
+    return updated
