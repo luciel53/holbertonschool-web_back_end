220e6f1eeb4583c300ba9b065e09a28e609bf0e5
luciel53 <l.leroty@gmail.com>
2023-11-07 21:18:09 +0100

    task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -19,7 +19,7 @@ numb_docs = collection.count_documents({})
 method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 for meth in method:
     numb_each_method = collection.count_documents({"method": meth})
-    print("{}: {}".format(meth, numb_each_method))
+    print(numb_each_method)
 
 get_count = collection.count_documents({"method": "GET", "path": "/status"})
-print("{}: {}".format(method, get_count))
+print(get_count)
