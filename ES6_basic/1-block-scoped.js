3ac8b2af8dd47635a1fbb028cbd4ea5ce559f061
luciel53 <l.leroty@gmail.com>
2023-11-08 15:18:58 +0100

    task 1 const

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,8 +2,8 @@ export default function taskBlock(trueOrFalse) {
   var task = false;
   var task2 = true;
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    const task = true;
+    const task2 = false;
 }
   return [task, task2];
 }
