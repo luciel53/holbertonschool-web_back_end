94017c3f2c5be9ac9096ad2e9952351dd3d2cbed
luciel53 <l.leroty@gmail.com>
2023-11-08 17:15:42 +0100

    task 1

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,9 +2,8 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
   if (trueOrFalse) {
-    /*eslint no-unused-vars: "error"*/
-    const task = true;
-    const task2 = false;
+    task = true;
+    task2 = false;
   }
   return [task, task2];
 }
