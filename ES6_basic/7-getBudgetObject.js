584ab7d4b243a4baf82d939beef3edeb69b72d08
luciel53 <l.leroty@gmail.com>
2023-11-10 09:47:23 +0100

    task 7

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,4 +1,4 @@
-export default function getBudgetObject(income=income, gdp=gdp, capita=capita) {
+export default function getBudgetObject(income, gdp, capita) {
 	const budget = {
 		income,
 		gdp,
