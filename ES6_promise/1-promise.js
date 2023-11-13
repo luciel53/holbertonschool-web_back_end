fd6b282794f7673762f40ff4103c24f2823ddcce
luciel53 <l.leroty@gmail.com>
2023-11-13 17:40:59 +0100

    task1 lint

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -4,9 +4,9 @@ export default function getFullResponseFromAPI(success) {
       resolve({
         status: 200,
         body: 'Success',
-      })
+      });
     } else {
-      reject("Error: The fake API is not working currently")
+      reject('Error: The fake API is not working currently');
     }
   });
 }
