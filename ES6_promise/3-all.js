f159d67b8e892763c2b40b5e2cc9e3e696d42f8a
luciel53 <l.leroty@gmail.com>
2023-11-13 19:52:01 +0100

    task 3 spaces

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from'./utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((response) => {
-      const {body} = response[0];
-      const {firstName} = response[1];
-      const {lastName} = response[2];
+      const { body } = response[0];
+      const { firstName } = response[1];
+      const { lastName } = response[2];
       console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => {
