cd9dd98ea60958493f6cf1842413f6be40e4b034
luciel53 <l.leroty@gmail.com>
2023-11-13 19:57:14 +0100

    task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -8,7 +8,5 @@ export default function handleProfileSignup() {
       const { lastName } = response[2];
       console.log(`${body} ${firstName} ${lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
