6df3572695b5ae4343063c65665f8d741c1a1a0d
luciel53 <l.leroty@gmail.com>
2023-11-13 17:03:18 +0100

    task 0 indentation

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export default function getResponseFromAPI() {
-	return new Promise((returnsomething) => {
-		returnsomething();
-	});
+  return new Promise((returnsomething) => {
+    returnsomething();
+  });
 }
