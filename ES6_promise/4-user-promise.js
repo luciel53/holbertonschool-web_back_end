b3b09f6236b75c84dbd324972fa4da3b32a8085b
luciel53 <l.leroty@gmail.com>
2023-11-14 11:16:19 +0100

    task 4

---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,14 @@
+export default function signUpUser(firstName, lastName) {
+  let promise = new Promise(function(resolve, reject) {
+  if (firstName && lastName) {
+    resolve({
+      firstName: firstName,
+      lastName: lastName,
+    });
+  }
+  else {
+    reject(new Error('The fake API is not working currently'));
+  }
+  });
+  return promise
+}
