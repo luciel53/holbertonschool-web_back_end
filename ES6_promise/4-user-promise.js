e5727463c2179e3dd2e447f5b0fea7cf192140d3
luciel53 <l.leroty@gmail.com>
2023-11-14 11:25:57 +0100

    task 4 lint

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -2,11 +2,10 @@ export default function signUpUser(firstName, lastName) {
   const promise = new Promise((resolve, reject) => {
     if (firstName && lastName) {
       resolve({
-        firstName: firstName,
-        lastName: lastName,
+        firstName,
+        lastName,
       });
-    }
-    else {
+    } else {
       reject(new Error('The fake API is not working currently'));
     }
   });
