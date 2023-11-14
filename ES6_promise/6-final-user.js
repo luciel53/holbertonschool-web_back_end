97c178e883693d7f6148739b8c39d9a1af645208
luciel53 <l.leroty@gmail.com>
2023-11-14 15:13:06 +0100

    task 6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignUp(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const file = uploadPhoto(fileName);
+
+  return Promise.all([user, file]).then((res) => {
+    return res.map(res => ({
+      status: res.status,
+      value: res.status === 'ok' ? res.value : res.console.error('ERROR'),
+    }))
+  });
+};
