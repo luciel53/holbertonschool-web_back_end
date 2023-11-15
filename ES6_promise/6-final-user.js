d7b3159cc049a968a86b98bf642562fe2d7eb18e
luciel53 <l.leroty@gmail.com>
2023-11-15 11:16:32 +0100

    task 7

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,7 +4,7 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignUp(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const file = uploadPhoto(fileName);
-
+  /* wait for all promises to be treated */
   return Promise.allSettled([user, file]).then((res) => res.map((res) => ({
     status: res.status,
     value: res.status === 'fulfilled' ? res.value : `Error: ${res.reason.message}`,
