5d56ce41f6537c7ece915b3ab16a66f614203f91
luciel53 <l.leroty@gmail.com>
2023-11-15 12:37:00 +0100

    task 8 deleted else

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -2,7 +2,6 @@ export default function divideFunction(numerator, denominator) {
   if (denominator !== 0) {
     const res = numerator / denominator;
     return res;
-  } else {
-    throw new Error('cannot divide by 0');
   }
+  throw new Error('cannot divide by 0');
 }
