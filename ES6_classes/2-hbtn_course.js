523793e7ae4b3e55ccdecbe012fd56a727a0c5c2
luciel53 <l.leroty@gmail.com>
2023-11-16 16:34:04 +0100

    task 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
@@ -36,6 +36,8 @@ export default class HolbertonCourse {
   set students(students) {
     if (Array.isArray(students) && students.every((student) => typeof student === 'string')) {
       this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
     }
   }
 }
