637dbd1ea91246960f39e45a2d4e086148973b18
luciel53 <l.leroty@gmail.com>
2023-11-16 15:09:11 +0100

    task 2 indentation

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,43 +1,41 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-	this._name = name;
-	this._length = length;
-	this._students = students;
+    this._name = name;
+    this._length = length;
+    this._students = students;
 
   }
   get name() {
-	return this._name;
+    return this._name;
   }
 
   set name(name) {
-	console.log("Setting name:", name);
-	if (typeof name === 'string') {
-		this._name = name;
+    if (typeof name === 'string') {
+    	this._name = name;
     } else {
-		throw new TypeError("Name must be a string");
+      throw new TypeError("Name must be a string");
     }
   }
 
   get length() {
-	return this._length;
+    return this._length;
   }
 
   set length(length) {
-	console.log("Setting length:", length);
-	if (typeof length === 'number') {
-		this._length = length;
-	} else {
-		throw new TypeError("Length must be a number");
-	}
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError("Length must be a number");
+    }
   }
 
   get students() {
-	return this._students;
+    return this._students;
   }
 
   set students(students) {
-	if (Array.isArray(students) && students.every(student => typeof student === 'string')) {
-		this._students = students;
-		}
+    if (Array.isArray(students) && students.every(student => typeof student === 'string')) {
+      this._students = students;
+    }
   }
 }
