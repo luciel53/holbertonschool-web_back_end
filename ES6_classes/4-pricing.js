2515e612424d077b699ed6c4664dd4a653215536
luciel53 <l.leroty@gmail.com>
2023-11-16 17:51:43 +0100

    task 4 add Currencyto set

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,9 +1,9 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-  constructor(amount, Currency) {
+  constructor(amount, currency) {
     this._amount = amount;
-    this._currency = Currency;
+    this._currency = currency;
   }
 
   get amount() {
@@ -19,7 +19,9 @@ export default class Pricing {
   }
 
   set currency(currency) {
-    this._currency = currency;
+    if (Currency) {
+      this._currency = currency;
+    }
   }
 
   displayFullPrice() {
