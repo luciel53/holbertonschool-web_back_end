df6b519471e96bb41adb7c58bb41ecc2bdba3c56
luciel53 <l.leroty@gmail.com>
2023-11-16 15:37:03 +0100

    task 2 arrow

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -34,7 +34,7 @@ export default class HolbertonCourse {
   }
 
   set students(students) {
-    if (Array.isArray(students) && students.every((student => typeof student === 'string'))) {
+    if (Array.isArray(students) && students.every((student) => typeof student === 'string')) {
       this._students = students;
     }
   }
