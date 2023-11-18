63fc40bf46229a9c19415eac62662dec27eef142
luciel53 <l.leroty@gmail.com>
2023-11-18 15:18:53 +0100

    task 10 indentation

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,8 +1,8 @@
 export default class Car {
   constructor(brand, motor, color) {
     this._brand = brand;
-	this._motor = motor;
-	this._color = color;
+    this._motor = motor;
+    this._color = color;
   }
 
   static get [Symbol.species] () {
