53b373d065bfd0671d3941d685ab2d6ad40cd1ec
luciel53 <l.leroty@gmail.com>
2023-11-20 13:22:42 +0100

    task 2 lint

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,3 @@
-import getListStudents from "./0-get_list_students";
-
 export default function getStudentsByLocation(getListStudents, city) {
   const cityStudents = getListStudents.filter((student) => student.location === city);
   return cityStudents;
