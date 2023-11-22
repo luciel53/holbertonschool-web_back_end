90800b52f896279c49e94cb5638f6305958e5b8e
luciel53 <l.leroty@gmail.com>
2023-11-22 16:52:43 +0100

    task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,10 @@
 export default function cleanSet(set, startString) {
   let text = '';
   set.forEach((value) => {
-    if (startString === '' || !startString || typeof value !== 'string' || typeof startString !== 'string') {
+    if (startString === '' || !startString || typeof startString !== 'string') {
       text = '';
-    } else if (value.startsWith(startString)) {
+    }
+    else if (typeof value === 'string' && value.includes(startString)) {
       const deletePartString = value.substring(startString.length);
       text += `${deletePartString}-`;
     }
