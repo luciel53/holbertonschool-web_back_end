5ae3ddfd2c3dd2f589978843ec1ec35696b5d5da
luciel53 <l.leroty@gmail.com>
2023-11-23 11:51:09 +0100

    task 1 lint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,8 +3,8 @@ const readline = require('readline').createInterface({
   output: process.stdout,
 });
 
-readline.question('Welcome to Holberton School, what is your name?\n', name => {
+readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
   console.log(`Your name is: ${name}`);
   readline.close();
-  console.log('This important software is now closing\n');
-})
+  console.log('This important software is now closing');
+});
