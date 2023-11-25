1c31455882d4605390eb2198df25ddb4ea3f5e42
luciel53 <l.leroty@gmail.com>
2023-11-25 20:47:33 +0100

    task 2 lint

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,11 +1,12 @@
 function countStudents(path) {
+  // eslint-disable-line global-require
   const fs = require('fs');
   try {
     const db = fs.readFileSync(path, 'utf8');
-    const byLine = db.split('\n').filter(line => line.trim() !== '');
+    const byLine = db.split('\n').filter((line) => line.trim() !== '');
     let totalStudents = 0;
-    let studentsByField = {};
-    for (let i = 1; i < byLine.length; i++) {
+    const studentsByField = {};
+    for (let i = 1; i < byLine.length; i += 1) {
       /* data line by line */
       const elements = byLine[i].split(',');
       /* select the field element and store it in a variable */
@@ -18,17 +19,17 @@ function countStudents(path) {
         if (studentsByField[field][firstname] === undefined) {
           studentsByField[field][firstname] = 1; // Add firstname to this field's list with count 1
         } else {
-          studentsByField[field][firstname]++; // Increment count for this firstname in this field
+          studentsByField[field][firstname] += 1; // Increment count for this firstname in this field
         }
       }
-      totalStudents++;
+      totalStudents += 1;
     }
 
     console.log(`Number of students: ${totalStudents}`);
-    for (let field in studentsByField) {
-      let firstnameByField = Object.keys(studentsByField[field]);
+    for (const field in studentsByField) {
+      const firstnameByField = Object.keys(studentsByField[field]);
       console.log(
-        `Number of students in ${field}: ${Object.values(studentsByField[field]).reduce((a, b) => a + b, 0)}. List: ${firstnameByField.join(', ')}`
+        `Number of students in ${field}: ${Object.values(studentsByField[field]).reduce((a, b) => a + b, 0)}. List: ${firstnameByField.join(', ')}`,
       );
     }
   } catch (error) {
