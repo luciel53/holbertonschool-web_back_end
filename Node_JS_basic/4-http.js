f33ff783875426d23289bb0d66789e99737a168e
luciel53 <l.leroty@gmail.com>
2023-11-26 14:30:18 +0100

    task 4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
 const app = http.createServer(function(req, res) {
-  console.log('Hello Holberton School!');
-});
-app.listen(1245);
+  res.write('Hello Holberton School!');
+  res.end();
+}).listen(1245);
+
+module.exports = app;
