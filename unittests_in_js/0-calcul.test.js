f3d454cc4314474e01a7d09d1d22d4582f8a4d00
luciel53 <l.leroty@gmail.com>
2023-11-28 17:37:11 +0100

    task 0 tests

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -5,4 +5,16 @@ describe('calculateNumber', () => {
   it('should return a rounded sum', () => {
     assert.strictEqual(calculateNumber(1, 2), 3);
   });
+
+  it('should return a rounded for first number float', () => {
+    assert.strictEqual(calculateNumber(2.8, 2), 5);
+  });
+
+  it('should return a rounded for second number float', () => {
+    assert.strictEqual(calculateNumber(3, 3.7), 7);
+  });
+
+  it('should return a rounded for both numbers float', () => {
+    assert.strictEqual(calculateNumber(1.2, 2.6), 4);
+  });
 })
