c9d27d5ce9e2dfd8dc245ec0dc19c125f4cadb22
luciel53 <l.leroty@gmail.com>
2023-11-28 19:09:44 +0100

    task 0

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -2,19 +2,15 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
-  it('should return a rounded sum', () => {
-    assert.strictEqual(calculateNumber(1, 2), 3);
-  });
-
-  it('should return a rounded for first number float', () => {
+  it('should return the rounded sum when first number is a float', () => {
     assert.strictEqual(calculateNumber(2.8, 2), 5);
   });
 
-  it('should return a rounded for second number float', () => {
+  it('should return the rounded sum when second number is a float', () => {
     assert.strictEqual(calculateNumber(1, 3.8), 5);
   });
 
-  it('should return a rounded for both numbers float', () => {
+  it('should return the rounded for both numbers float', () => {
     assert.strictEqual(calculateNumber(1.2, 2.6), 4);
   });
 })
