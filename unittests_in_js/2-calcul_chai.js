fba23f557235528fc2a68ddac577116613753b36
luciel53 <l.leroty@gmail.com>
2023-11-29 15:31:24 +0100

    task 2

--- unittests_in_js/2-calcul_chai.js
+++ unittests_in_js/2-calcul_chai.js
@@ -1,20 +1,15 @@
 function calculateNumber(type, a, b) {
-	const roundedA = Math.round(a);
-	const roundedB = Math.round(b);
+    switch (type) {
+      case 'SUM':
+        return Math.round(a) + Math.round(b);
+      case 'SUBTRACT':
+        return Math.round(a) + Math.round(b);
+      case 'DIVIDE':
+        if (Math.round(b) === 0) {
+          return 'Error';
+        }
+        return Math.round(a) / Math.round(b);
+    }
+}
 
-	switch (type) {
-	  case 'SUM':
-		return roundedA + roundedB;
-
-	  case 'SUBTRACT':
-		return roundedA - roundedB;
-
-	  case 'DIVIDE':
-		if (roundedB === 0) {
-		  return 'Error';
-		}
-		return roundedA / roundedB;
-	}
-  }
-
-  module.exports = calculateNumber;
+module.exports = calculateNumber;
