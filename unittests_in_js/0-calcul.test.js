846a11b2aba6b9c79ff97a104d0ee3eb6f596748
luciel53 <l.leroty@gmail.com>
2023-11-29 12:25:58 +0100

    task 0

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -8,7 +8,7 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(3.7, 1.5), 6);
+    assert.strictEqual(calculateNumber(3.7, 1.2), 5);
     assert.strictEqual(calculateNumber(3.7, 1), 5);
   });
 
