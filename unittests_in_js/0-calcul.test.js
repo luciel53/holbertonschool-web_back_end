1f61024340686f375825ac5d886cd340a50ed32f
luciel53 <l.leroty@gmail.com>
2023-11-29 11:56:41 +0100

    task 0 test

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -15,8 +15,5 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1, -1), 0);
     assert.strictEqual(calculateNumber(-1, -1), -2);
   });
-  it('checks arguments', () => {
-    assert.strictEqual(isNaN(calculateNumber(1)), true);
-    assert.strictEqual(isNaN(calculateNumber()), true);
-  });
+  
 });
