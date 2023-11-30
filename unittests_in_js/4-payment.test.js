62ddf1772c976e503a8798f2544742f22b2fb7fb
luciel53 <l.leroty@gmail.com>
2023-11-30 16:42:14 +0100

    task 4

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -5,14 +5,13 @@ const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
 describe('sendPaymentRequestToApi to test', () => {
-  let stubCalcNum = sinon.stub(Utils, 'calculateNumber').returns(10);
   let spyCalcNum = sinon.spy(console, 'log');
   it('should call Utils.calculateNumber with type = SUM, a = 100, and b = 20', () => {
-    const res = sendPaymentRequestToApi(100, 20);
+    let stubCalcNum = sinon.stub(Utils, 'calculateNumber').returns(10);
+    sendPaymentRequestToApi(100, 20);
     expect(stubCalcNum.calledWithExactly('SUM', 100, 20)).to.equal(true);
     expect(spyCalcNum.calledWithExactly('The total is: 10')).to.be.true;
-    expect(Utils.calculateNumber('SUM', 100, 20)).to.equal(res);
-	spyCalcNum.restore();
-	stubCalcNum = restore();
+    spyCalcNum.restore();
+    stubCalcNum = restore();
   });
 });
