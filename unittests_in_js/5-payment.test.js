c35b423027d16958a1673c711fbfde8e9c672a32
luciel53 <l.leroty@gmail.com>
2023-12-02 19:42:44 +0100

    task 5

--- unittests_in_js/5-payment.test.js
+++ unittests_in_js/5-payment.test.js
@@ -14,14 +14,16 @@ describe('sendPaymentRequestToApi to test', () => {
     spy.restore();
   });
 
-  it('should() send payment request with 100 and 20', () => {
+  it('should send payment request with 100 and 20', () => {
     const result = sendPaymentRequestToApi(100, 20);
     expect(result).to.equal(120);
     expect(spy.calledOnceWithExactly('The total is: 120')).to.be.true;
+    expect(spy.calledOnce).to.be.true;
   });
-  it('should() send payment request with 10 and 10', () => {
+  it('should send payment request with 10 and 10', () => {
     const result = sendPaymentRequestToApi(10, 10);
     expect(result).to.equal(20);
     expect(spy.calledOnceWithExactly('The total is: 20')).to.be.true;
+    expect(spy.calledOnce).to.be.true;
   });
 });
