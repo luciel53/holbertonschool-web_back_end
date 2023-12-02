4c9a9f1fa68ceaeccdcac2436d617f936d9d6009
luciel53 <l.leroty@gmail.com>
2023-12-02 18:10:26 +0100

    task 5

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,26 @@
+const sinon = require('sinon');
+const chai = require('chai');
+const { expect } = chai;
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi to test', () => {
+  beforeEach(() => {
+    let spy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    spy.restore();
+  });
+
+  it('should() send payment request with 100 and 20', () => {
+    const result = sendPaymentRequestToApi(100, 20);
+    expect(result).to.equal(120);
+    expect(spy.calledOnceWithExactly('The total is: 120')).to.be.true;
+  });
+  it('should() send payment request with 10 and 10', () => {
+    const result = sendPaymentRequestToApi(10, 10);
+    expect(result).to.equal(20);
+    expect(spy.calledOnceWithExactly('The total is: 20')).to.be.true;
+  });
+});
